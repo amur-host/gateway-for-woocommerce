f3981ef228b19cbb9f17f58d847cb59269e62213
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2018-08-22 03:13:54 +1000

    WC_Amur_gateway if (class_exist)

--- waves-gateway-for-woocommerce.php
+++ waves-gateway-for-woocommerce.php
@@ -1,20 +1,18 @@
 <?php
 
 /**
- * Waves Gateway for Woocommerce
+ * Amur Gateway for Woocommerce
  *
- * Plugin Name: WNET Gateway for Woocommerce (also for other Waves assets)
- * Plugin URI: https://github.com/wavesnode/gateway-for-woocommerce/
- * Description: Show prices in Waves (or asset) and accept Waves payments in your woocommerce webshop
+ * Plugin Name: WNET Gateway for Woocommerce (also for other Amur assets)
+ * Plugin URI: https://github.com/amur-host/gateway-for-woocommerce
+ * Description: Show prices in Amur (or asset) and accept Amur payments in your woocommerce webshop
  * Version: 0.4.4
- * Author: Wavesnode.NET / Useless Waves Token
- * Author URI:   https://wavesnode.net/blog/waves-woocommerce-gateway/
+ * Author URI:   https://github.com/amur-host/gateway-for-woocommerce
  * License: GPLv2 or later
  * License URI: http://www.opensource.org/licenses/gpl-license.php
- * Text Domain: waves-gateway-for-woocommerce
+ * Text Domain: amur-gateway-for-woocommerce
  * Domain Path: /languages/
   *
- * Copyright 2018 Wavesnode.NET / Copyright 2017 Useless Waves Token Foundation
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -35,9 +33,9 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-if (!class_exists('WcWaves')) {
+if (!class_exists('WcAmur')) {
 
-    class WcWaves
+    class WcAmur
     {
 
         private static $instance;
@@ -53,7 +51,7 @@ if (!class_exists('WcWaves')) {
         	self::$plugin_url = plugin_dir_url(self::$plugin_basename);
             add_action('plugins_loaded', array($this, 'init'));
         }
-        
+
         public static function getInstance()
         {
             if (null === self::$instance) {
@@ -74,7 +72,7 @@ if (!class_exists('WcWaves')) {
                 return;
             }
 
-            if (class_exists('WC_Waves_Gateway')) {
+            if (class_exists('WC_Amur_Gateway')) {
 	            return;
 	        }
 
@@ -85,33 +83,33 @@ if (!class_exists('WcWaves')) {
 	        include_once plugin_basename('includes/base58/src/ServiceInterface.php');
 	        include_once plugin_basename('includes/base58/src/GMPService.php');
 	        include_once plugin_basename('includes/base58/src/BCMathService.php');
-	        include_once plugin_basename('includes/class-waves-gateway.php');
-	        include_once plugin_basename('includes/class-waves-api.php');
-	        include_once plugin_basename('includes/class-waves-exchange.php');
-	        include_once plugin_basename('includes/class-waves-settings.php');
-	        include_once plugin_basename('includes/class-waves-ajax.php');
+	        include_once plugin_basename('includes/class-amur-gateway.php');
+	        include_once plugin_basename('includes/class-amur-api.php');
+	        include_once plugin_basename('includes/class-amur-exchange.php');
+	        include_once plugin_basename('includes/class-amur-settings.php');
+	        include_once plugin_basename('includes/class-amur-ajax.php');
 
 	        add_filter('woocommerce_payment_gateways', array($this, 'addToGateways'));
-            add_filter('woocommerce_currencies', array($this, 'WavesCurrencies'));
-            add_filter('woocommerce_currency_symbol', array($this, 'WavesCurrencySymbols'), 10, 2);
+            add_filter('woocommerce_currencies', array($this, 'AmurCurrencies'));
+            add_filter('woocommerce_currency_symbol', array($this, 'AmurCurrencySymbols'), 10, 2);
 
-	        add_filter('woocommerce_get_price_html', array($this, 'WavesFilterPriceHtml'), 10, 2);
-	        add_filter('woocommerce_cart_item_price', array($this, 'WavesFilterCartItemPrice'), 10, 3);
-	        add_filter('woocommerce_cart_item_subtotal', array($this, 'WavesFilterCartItemSubtotal'), 10, 3);
-	        add_filter('woocommerce_cart_subtotal', array($this, 'WavesFilterCartSubtotal'), 10, 3);
-	        add_filter('woocommerce_cart_totals_order_total_html', array($this, 'WavesFilterCartTotal'), 10, 1);
+	        add_filter('woocommerce_get_price_html', array($this, 'AmurFilterPriceHtml'), 10, 2);
+	        add_filter('woocommerce_cart_item_price', array($this, 'AmurFilterCartItemPrice'), 10, 3);
+	        add_filter('woocommerce_cart_item_subtotal', array($this, 'AmurFilterCartItemSubtotal'), 10, 3);
+	        add_filter('woocommerce_cart_subtotal', array($this, 'AmurFilterCartSubtotal'), 10, 3);
+	        add_filter('woocommerce_cart_totals_order_total_html', array($this, 'AmurFilterCartTotal'), 10, 1);
 
 	    }
 
 	    public static function addToGateways($gateways)
 	    {
-	        $gateways['waves'] = 'WcWavesGateway';
+	        $gateways['amur'] = 'WcAmurGateway';
 	        return $gateways;
 	    }
 
-        public function WavesCurrencies( $currencies )
+        public function AmurCurrencies( $currencies )
         {
-            $currencies['WAVES'] = __( 'Waves', 'waves' );
+            $currencies['AMUR'] = __( 'Amur', 'amur' );
             $currencies['WNET'] = __( 'Wavesnode.NET', 'wnet' );
             $currencies['ARTcoin'] = __( 'ARTcoin', 'ARTcoin' );
             $currencies['POL'] = __( 'POLTOKEN.PL', 'POL' );
@@ -122,9 +120,9 @@ if (!class_exists('WcWaves')) {
             return $currencies;
         }
 
-        public function WavesCurrencySymbols( $currency_symbol, $currency ) {
+        public function AmurCurrencySymbols( $currency_symbol, $currency ) {
             switch( $currency ) {
-                case 'WAVES': $currency_symbol = 'WAVES'; break;
+                case 'AMUR': $currency_symbol = 'AMUR'; break;
                 case 'WNET': $currency_symbol = 'WNET'; break;
                 case 'ARTcoin': $currency_symbol = 'ARTcoin'; break;
                 case 'POL': $currency_symbol = 'POL'; break;
@@ -136,48 +134,48 @@ if (!class_exists('WcWaves')) {
             return $currency_symbol;
         }
 
-	    public function WavesFilterCartTotal($value)
+	    public function AmurFilterCartTotal($value)
 	    {
-	        return $this->convertToWavesPrice($value, WC()->cart->total);
+	        return $this->convertToAmurPrice($value, WC()->cart->total);
 	    }
 
-	    public function WavesFilterCartItemSubtotal($cart_subtotal, $compound, $that)
+	    public function AmurFilterCartItemSubtotal($cart_subtotal, $compound, $that)
 	    {
-	        return $this->convertToWavesPrice($cart_subtotal, $that->subtotal);
+	        return $this->convertToAmurPrice($cart_subtotal, $that->subtotal);
 	    }
 
-	    public function WavesFilterPriceHtml($price, $that)
+	    public function AmurFilterPriceHtml($price, $that)
 	    {
-	        return $this->convertToWavesPrice($price, $that->price);
+	        return $this->convertToAmurPrice($price, $that->price);
 	    }
 
-	    public function WavesFilterCartItemPrice($price, $cart_item, $cart_item_key)
+	    public function AmurFilterCartItemPrice($price, $cart_item, $cart_item_key)
 	    {
 	        $item_price = ($cart_item['line_subtotal'] + $cart_item['line_subtotal_tax']) / $cart_item['quantity'];
-	        return $this->convertToWavesPrice($price,$item_price);
+	        return $this->convertToAmurPrice($price,$item_price);
 	    }
 
-	    public function WavesFilterCartSubtotal($price, $cart_item, $cart_item_key)
+	    public function AmurFilterCartSubtotal($price, $cart_item, $cart_item_key)
 	    {
 	        $subtotal = $cart_item['line_subtotal'] + $cart_item['line_subtotal_tax'];
-	        return $this->convertToWavesPrice($price, $subtotal);
+	        return $this->convertToAmurPrice($price, $subtotal);
 	    }
 
-	    private function convertToWavesPrice($price_string, $price)
+	    private function convertToAmurPrice($price_string, $price)
 	    {
-            $options = get_option('woocommerce_waves_settings');
-            if(!in_array(get_woocommerce_currency(), array("WAVES","WNET","ARTcoin","POL","Wykop Coin","Surfcash","TN","Ecop")) && $options['show_prices'] == 'yes') {
-                $waves_currency = $options['asset_code'];
-                if(empty($waves_currency)) {
-                    $waves_currency = 'Waves';
+            $options = get_option('woocommerce_amur_settings');
+            if(!in_array(get_woocommerce_currency(), array("AMUR","WNET","ARTcoin","POL","Wykop Coin","Surfcash","TN","Ecop")) && $options['show_prices'] == 'yes') {
+                $amur_currency = $options['asset_code'];
+                if(empty($amur_currency)) {
+                    $amur_currency = 'Amur';
                 }
-                $waves_assetId = $options['asset_id'];
-                if(empty($waves_assetId)) {
-                    $waves_assetId = null;
+                $amur_assetId = $options['asset_id'];
+                if(empty($amur_assetId)) {
+                    $amur_assetId = null;
                 }
-                $waves_price = WavesExchange::convertToAsset(get_woocommerce_currency(), $price,$waves_assetId);
-                if ($waves_price) {
-                    $price_string .= '&nbsp;(<span class="woocommerce-price-amount amount">' . $waves_price . '&nbsp;</span><span class="woocommerce-price-currencySymbol">'.$waves_currency.')</span>';
+                $amur_price = AmurExchange::convertToAsset(get_woocommerce_currency(), $price,$amur_assetId);
+                if ($amur_price) {
+                    $price_string .= '&nbsp;(<span class="woocommerce-price-amount amount">' . $amur_price . '&nbsp;</span><span class="woocommerce-price-currencySymbol">'.$amur_currency.')</span>';
                 }
             }
 	        return $price_string;
@@ -186,10 +184,10 @@ if (!class_exists('WcWaves')) {
 
 }
 
-WcWaves::getInstance();
+WcAmur::getInstance();
 
-function wavesGateway_textdomain() {
-    load_plugin_textdomain( 'waves-gateway-for-woocommerce', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+function AmurGateway_textdomain() {
+    load_plugin_textdomain( 'amur-gateway-for-woocommerce', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
 }
-        
-add_action( 'plugins_loaded', 'wavesGateway_textdomain' );
+
+add_action( 'plugins_loaded', 'amurGateway_textdomain' );
